d43f13685c18eec59916c7bae91fa7b32cd3eef0
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-19 22:13:10 +0300

    Added util.Scanner and util.ArrayList

--- src/src/Animals.java
+++ src/src/Animals.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Animals {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        ArrayList<String> arr = new ArrayList<>();
 
     }
 }
