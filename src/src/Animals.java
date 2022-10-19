30b9423e4aeae1be1df58713e91ac0384bbc31a2
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-19 22:31:50 +0300

    Added do-while, menu, in.nextInt()

--- src/src/Animals.java
+++ src/src/Animals.java
@@ -14,5 +14,21 @@ public class Animals {
         boolean quit = false;
 
         int menuItem;
+
+        do {
+            System.out.println("");             // empty string
+            System.out.println("Menu:");
+            System.out.println("1. Show animals list");
+            System.out.println("2. Add new animal to list");
+            System.out.println("3. Exit");
+            System.out.println("Select menu: ");
+
+
+            menuItem = in.nextInt();
+
+        } while (!quit);
+
+        System.out.println("Bye-bye");
+
     }
 }
